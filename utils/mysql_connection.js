2e1e0fb00fcf75eebc674d2ab0c8892618638ade
Natanhel Poliszuk <natanhelp@gmail.com>
2022-05-16 22:57:18 +0300

    Added good practices - Updated .gitignore - Added .env file

--- utils/mysql_connection.js
+++ utils/mysql_connection.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  port: '3306',
-  user: "root",
-  password: "MyNewPass",
-  database: "Taboola"
+  host: process.env.MYSQL_URL,
+  port: '3306',  
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 exports.connection = con;
